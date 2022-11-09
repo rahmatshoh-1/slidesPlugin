882e14cbfebb26c1717bcebd3198e832cb0093cb
unknown <rahmatshoh.rahmonov@mail.ru>
2022-11-09 13:52:29 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function slidesPlugin(activeSlide) {
+  const slides = document.querySelectorAll(".slide");
+  slides[activeSlide].classList.add("active");
+  slides.forEach((slide) => {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  });
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
+}
+slidesPlugin(2);
